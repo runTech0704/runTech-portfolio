b1ae41d828f92a2924468313785190fac199f256
runrun0704 <satoru0704.f@gmail.com>
2024-12-31 16:52:10 +0900

    Create draft topComponents

--- react-vite/src/components/Top.jsx
+++
@@ -1,7 +0,0 @@
-const Top = () => {
-    return (
-        <h3>Top</h3>
-    )
-};
-
-export default Top;
---
+++ react-vite/src/components/top/Top.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./top.css";
+
+
+const Top = () => {
+    return (
+        <>
+            <div className="top-container">
+                <h1>demo</h1>
+            </div>
+        </>
+    )
+};
+
+export default Top;
