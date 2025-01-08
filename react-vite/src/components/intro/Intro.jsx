abbb97ee5013431c5c0d61d6a57c60f8ed5fae77
runrun0704 <satoru0704.f@gmail.com>
2025-01-09 08:17:53 +0900

    Create Intro-comp base

---
+++ react-vite/src/components/intro/Intro.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Intro = () => {
+    return (
+        <>
+            <div className="intro-container">
+                <div className="introduction">
+                    <h2>runTechとは</h2>
+                    <p>braaaa braaaaa</p>
+                </div>
+                <div className="introduce-card">
+                    <h2>Author</h2>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Intro;
