962267a4c874064a38ca2a05bd4cf7c4d7a3cce4
runrun0704 <satoru0704.f@gmail.com>
2025-01-19 07:21:32 +0900

    First commit

---
+++ react-vite/src/components/about-me/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const AboutMe = () => {
+    return (
+        <div className="about-me-container">
+                <h1 className="head">AboutMe</h1>
+        </div>
+    )
+};
+
+export default AboutMe;
